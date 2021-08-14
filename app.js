f8226c49135cf150b3331f7ef144443835f04960
Daniel Reis <danielreisp.2020@gmail.com>
2021-08-13 21:20:06 -0300

    criando funcao que permite filtrar as despesas

--- app.js
+++ app.js
@@ -65,6 +65,54 @@ class Bd {
 
 		return despesas
 	}
+
+	pesquisar(despesa){
+
+		let despesasFiltradas = Array()
+		despesasFiltradas = this.recuperarTodosRegistros()
+		console.log(despesasFiltradas);
+		console.log(despesa)
+
+		//ano
+		if(despesa.ano != ''){
+			console.log("filtro de ano");
+			despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+		}
+			
+		//mes
+		if(despesa.mes != ''){
+			console.log("filtro de mes");
+			despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+		}
+
+		//dia
+		if(despesa.dia != ''){
+			console.log("filtro de dia");
+			despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+		}
+
+		//tipo
+		if(despesa.tipo != ''){
+			console.log("filtro de tipo");
+			despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+		}
+
+		//descricao
+		if(despesa.descricao != ''){
+			console.log("filtro de descricao");
+			despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+		}
+
+		//valor
+		if(despesa.valor != ''){
+			console.log("filtro de valor");
+			despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+		}
+
+		
+		return despesasFiltradas
+
+	}
 }
 
 let bd = new Bd()
@@ -100,8 +148,14 @@ function cadastrarDespesa() {
 
 		//dialog de sucesso
 		$('#modalRegistraDespesa').modal('show') 
-		
 
+		ano.value = '' 
+		mes.value = ''
+		dia.value = ''
+		tipo.value = ''
+		descricao.value = ''
+		valor.value = ''
+		
 	} else {
 		
 		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
@@ -115,11 +169,12 @@ function cadastrarDespesa() {
 	}
 }
 
-function carregaListaDespesas() {
+function carregaListaDespesas(despesas = Array(), filtro = false) {
 
-	let despesas = Array()
-
-	despesas = bd.recuperarTodosRegistros() 
+    if(despesas.length == 0 && filtro == false){
+		despesas = bd.recuperarTodosRegistros() 
+	}
+	
 
 	/*
 
@@ -133,7 +188,7 @@ function carregaListaDespesas() {
 	*/
 
 	let listaDespesas = document.getElementById("listaDespesas")
-
+    listaDespesas.innerHTML = ''
 	despesas.forEach(function(d){
 
 		//Criando a linha (tr)
@@ -161,5 +216,23 @@ function carregaListaDespesas() {
 		linha.insertCell(3).innerHTML = d.valor
 		console.log(d)
 	})
+
+ }
+
+ 
+ function pesquisarDespesa(){
+	 
+	let ano  = document.getElementById("ano").value
+	let mes = document.getElementById("mes").value
+	let dia = document.getElementById("dia").value
+	let tipo = document.getElementById("tipo").value
+	let descricao = document.getElementById("descricao").value
+	let valor = document.getElementById("valor").value
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+	let despesas = bd.pesquisar(despesa)
+	 
+	this.carregaListaDespesas(despesas, true)
 
  }
